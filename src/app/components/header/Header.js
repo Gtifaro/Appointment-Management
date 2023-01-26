174693c9f39b734e03680603fec81d3da12778c8
Gabriel Rodríguez <gabrieltifaro2021@gmail.com>
2023-01-25 20:30:44 -0500

    Added home route

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 
 import { Medic } from "../medic/Medic";
+import { Home } from "../home/Home";
 
 export const Header = () => {
     return (
@@ -10,10 +11,12 @@ export const Header = () => {
                 <div className='OptionsContainer'>
                     <NavLink title="Home" className={(data) => data.isActive ? 'NavLink-Active': 'NavLink'} to="/">Home</NavLink>
                     <NavLink title="Medics" className={(data) => data.isActive ? 'NavLink-Active': 'NavLink'} to="/Medics">Medics</NavLink>
+                    <NavLink title="Medics" className={(data) => data.isActive ? 'NavLink-Active': 'NavLink'} to="/Home">Home</NavLink>
                 </div>
                 <Routes>
                     <Route path="/" element={<h1>Home</h1>}/>
                     <Route path="/Medics" element={<Medic/>}/>
+                    <Route path="/Home" element={<Home/>}/>
                 </Routes>
             </Router>
         </div>
